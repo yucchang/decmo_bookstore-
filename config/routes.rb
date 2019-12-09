ba98cc625e0fa9437fbc51f84b2fb64e30626848
Yu-Cheng Chang <yu-chengchang@huangyuqingde-MacBook-Pro.local>
2019-12-09 12:16:35 +0800

    google pauth(WIP)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { 
+    omniauth_callbacks: 'users/omniauth_callbacks' 
+  }
+  
   resources :books, only: [:index, :show]
   root 'books#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_024253) do
+ActiveRecord::Schema.define(version: 2019_12_09_040033) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2019_12_09_024253) do
     t.string "role", default: "user"
     t.string "provider"
     t.string "uid"
+    t.string "avatar"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
